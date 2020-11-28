f079d4629d1b61d7e68b4b20ed41a327a5ab1f6a
ueno <ueno.kanagawa@gmail.com>
2020-11-28 11:45:45 +0900

    feat:import alias

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import Layout from '../../components/layout';
+import Layout from '@/components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import { GetStaticProps, GetStaticPaths } from 'next';
 
