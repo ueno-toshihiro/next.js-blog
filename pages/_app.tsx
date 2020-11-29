0fbcbfb97302b3067d8e1626dc96ec5ab95afaa4
ueno <ueno.kanagawa@gmail.com>
2020-11-29 15:53:55 +0900

    feat:watch performance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,13 @@ import { AppProps } from 'next/app';
 import '../styles/global.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log({ pageProps });
   return <Component {...pageProps} />;
+}
+
+// パフォーマンス測定
+export function reportWebVitals(metric) {
+  console.log(`${metric.name}::`, metric);
+  // google analytics
+  // https://nextjs.org/docs/advanced-features/measuring-performance#sending-results-to-analytics
 }
