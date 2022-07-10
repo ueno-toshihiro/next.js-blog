415dd64d58cb9190a9e39d100faaffbed4558525
ueno toshihiro <ueno.hub@gmail.com>
2022-07-10 22:31:38 +0900

    change blob to image base64

--- components/ui/image-uploader/image-uploader.tsx
+++ components/ui/image-uploader/image-uploader.tsx
@@ -15,35 +15,27 @@ const borderDragStyle = {
 const ImageUploader: React.FC = () => {
   const [imageUrls, setImageUrls] = React.useState([]);
 
-  const onDropFiles = (acceptedFiles: File[]) => {
-    const uploadFiles = acceptedFiles.map((file) => {
-
+  const onDropFiles = async (acceptedFiles: File[]) => {
+    const promisesBase64 = acceptedFiles.map((file) => {
       // NOTE: About BLOB https://ja.javascript.info/blob
       /**
-       * URL.createObjectURL により生成された各 URL に対して、ブラウザは url → blob のマッピングを内部に格納します。そのため、url は短いですが、blob にアクセスすることができます。
-       * 生成された url (とそれへのリンク)は現在のドキュメント内で、かつ開いている間のみ有効です。それにより、<img>, <a> 、基本的に url を想定するあらゆるオブジェクトで blob を参照することができます。
-       * しかし、副作用もあります。blob のマッピングがある間、blob 自身はメモリ内に存在し続けます。ブラウザはそれを解放することはできません。
-       * マッピングは、ドキュメントがアンロードされると自動的にクリアされ、blob もそのとき解放されます。しかしアプリケーションの寿命が長ければ、すぐにはそれは起きません。
-       * したがって、URL を作成すると、それ以上必要なくなっても、blob はメモリに溜まります。
-       * URL.revokeObjectURL(url) は内部のマッピングから参照を削除します。これにより blob が削除され(他に参照がない場合)、メモリを解放することができます。
-       * 最後の例では、blob は即時ダウンロードのために、一度だけ使われることを意図しているので、すぐに URL.revokeObjectURL(link.href) を呼び出します。
-       * しかし、クリック可能な HTML リンクのある前の例では、URL.revokeObjectURL(link.href) を呼び出しません。なぜなら、これは blob url を無効にするからです。失効後は、マッピングが削除されているので、url は機能しなくなります。
+       * URL.createObjectURL の代替は blob を base64 エンコードされた文字列に変換する方法です。
+       * このエンコーディングは、0 から 64 までの ASCII コードを用いて、バイナリデータを安全で “読み出し可能な” 文字列として表現します。そしてより重要なことは、“data-url” でこのエンコーディングが使用できることです。
+       * data url は data:[<mediatype>][;base64],<data> の形式です。このような url は “通常の” url と同等に、あらゆる場所で使用することができます。
        */
-      const createObjectURL = (window.URL || window.webkitURL).createObjectURL;
+      let blob = new Blob([file], { type: file.type });
+      let reader = new FileReader();
+      reader.readAsDataURL(blob); // blob を base64 へ変換し onload を呼び出します
 
-      if (acceptedFiles.length != 0) {
-        const images = acceptedFiles.map((file) => createObjectURL(file))
-        setImageUrls(images);
-      };
-
-      return {
-        file,
-        id: v4(),
-        uploadProgress: 0,
-      };
+      return new Promise((resolve) => {
+        reader.onload = function() {
+          resolve(reader.result);
+        };
+      });
     });
-    
-    console.log('@@', { uploadFiles });
+
+    const images = await Promise.all(promisesBase64);
+    setImageUrls(images);
   };
 
   const { getRootProps, getInputProps, isDragActive, open, acceptedFiles } = useDropzone({
@@ -59,12 +51,6 @@ const ImageUploader: React.FC = () => {
   const style = React.useMemo(() => (
       { ...(isDragActive ? borderDragStyle : borderNormalStyle)}
   ), [isDragActive]);
-
-  React.useEffect(() => {
-    return () => {
-      imageUrls.forEach((file) => (window.URL || window.webkitURL).revokeObjectURL(file));
-    };
-  }, [imageUrls]);
 
   return (
     <>
