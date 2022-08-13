82e8f0afb46dcfbb875e8983e7b11fc345ce512d
ueno <ueno.hub@gmail.com>
2022-08-14 00:17:20 +0900

    Add dot env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 
 /**
@@ -11,5 +12,14 @@ module.exports = {
   generateBuildId: async () => {
     // 例えば、ここで最新のコミットハッシュを取得することができます
     return 'my-build-id';
+  },
+  env: {
+    // Reference a variable that was defined in the .env file and make it available at Build Time
+    REGION: process.env.REGION,
+    USER_POOL_ID: process.env.USER_POOL_ID,
+    USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID,
+    OAUTH_DOMAIN: process.env.OAUTH_DOMAIN,
+    OAUTH_REDIRECT_SIGN_IN: process.env.OAUTH_REDIRECT_SIGN_IN,
+    OAUTH_REDIRECT_SIGN_OUT: process.env.OAUTH_REDIRECT_SIGN_OUT,
   },
 };
